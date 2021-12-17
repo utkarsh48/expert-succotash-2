1fa0fb4f0e669486642ff681d7277c2ecdbb581b
Utkarsh Dhiman <47657465+utkarsh48@users.noreply.github.com>
2021-12-17 05:47:10 +0000

    add: step2

---
+++ src/components/Step2.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import "../assets/css/Step.css";
+
+
+export default function Step2(props){
+  const {hasHeaders, type, encoding, delimiter, onHasHeadersChange, onTypeChange, onEncodingChange, onDelimiterChange} = props;
+
+  return (
+    <div className='step-2 step-container step-shadow'>
+      <div className='step-no'>Step 2:</div>
+      <div className='step-content'>
+        <div className='step-content-heading'>Specify Format</div>
+        <div className='step-series'>
+          <div>
+            <label className='block'>File Type</label>
+          </div>
+          <div>
+            <select name="f-type" onChange={e=>onTypeChange(e)} type={type}>
+              <option value="text/csv">CSV</option>
+              <option value="application/json">JSON</option>
+            </select>
+          </div>
+        </div>
+        <div className='step-series'>
+          <div>
+            <label className='block'>Character Encoding</label>
+          </div>
+          <div>
+            <select name="encoding" onChange={e=>onEncodingChange(e)} value={encoding}>
+              <option value="UTF-8">UTF-8</option>
+              <option value="UTF-16">UTF-16</option>
+              <option value="UTF-32">UTF-32</option>
+            </select>
+          </div>
+        </div>
+        <div className='step-series'>
+          <div>
+            <label className='block'>Delimiter</label>
+          </div>
+          <div>
+            <select value={delimiter} onChange={e=>onDelimiterChange(e)} name="delimiter">
+              <option value=",">comma</option>
+              <option value=";">semi-colon</option>
+            </select>
+          </div>
+        </div>
+        <div className='step-series'>
+          <div>
+            <label className='block'>Has Header</label>
+          </div>
+          <div>
+            <input name="has-header" checked={hasHeaders} onChange={e=>onHasHeadersChange(e)} type="checkbox" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
